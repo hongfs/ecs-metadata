6aec29ce85df84803a81e6566a9d0cac935127ba
hongfs <hong@hongfs.cn>
2023-10-16 10:41:13 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hongfs/ecs-metadata
+
+go 1.21.0
---
+++ pkg/metadata/metadata.go
@@ -0,0 +1,111 @@
+package metadata
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"time"
+)
+
+// Hostname 获取主机名
+func Hostname() string {
+	hostname, _ := request("hostname")
+	return hostname
+}
+
+// ID 获取实例ID
+func ID() string {
+	id, _ := request("instance-id")
+	return id
+}
+
+// Region 获取实例所在地域，如 cn-hangzhou
+func Region() string {
+	region, _ := request("region-id")
+	return region
+}
+
+// Zone 获取实例所在可用区，如 cn-hangzhou-b
+func Zone() string {
+	zone, _ := request("zone-id")
+	return zone
+}
+
+type RamInfo struct {
+	AccessKeyID     string    `json:"AccessKeyId"`
+	AccessKeySecret string    `json:"AccessKeySecret"`
+	Expiration      time.Time `json:"Expiration"`
+	SecurityToken   string    `json:"SecurityToken"`
+	LastUpdated     time.Time `json:"LastUpdated"`
+	Code            string    `json:"Code"`
+}
+
+func Ram(name string) *RamInfo {
+	data, err := request("ram/security-credentials/" + name)
+
+	if err != nil {
+		return nil
+	}
+
+	ram := RamInfo{}
+
+	err = json.Unmarshal([]byte(data), &ram)
+
+	if err != nil {
+		return nil
+	}
+
+	return &ram
+}
+
+// TerminationTime 获取实例释放时间，仅抢占式适用
+func TerminationTime() time.Time {
+	defaultTime := time.Date(9999, 12, 31, 0, 0, 0, 0, time.UTC)
+
+	terminationTime, err := request("termination-time")
+
+	if err != nil {
+		return defaultTime
+	}
+
+	t, err := time.Parse("2006-01-02T15:04:05Z", terminationTime)
+
+	if err != nil {
+		return defaultTime
+	}
+
+	return t
+}
+
+func request(path string) (string, error) {
+	req, err := http.NewRequest(http.MethodGet, "http://100.100.100.200/latest/meta-data/"+path, nil)
+
+	if err != nil {
+		return "", err
+	}
+
+	client := http.Client{
+		Timeout: 1 * time.Second,
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New("request failed")
+	}
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
